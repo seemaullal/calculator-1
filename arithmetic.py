d1e750ccca6928e49f914aff307f8da4cd982c5c
Aubrey <aubrey.sandrs@gmail.com>
2021-03-15 23:04:06 +0000

    Fixed arithmetic functions to all have a return

--- arithmetic.py
+++ arithmetic.py
@@ -7,31 +7,31 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-    num1 - num2
+    return num1 - num2
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-    num1 * num2
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-    num1/num2
+    return num1/num2
 
 def square(num1):
     """Return the square of the input."""
-    num1**2
+    return num1**2
 
 def cube(num1):
     """Return the cube of the input."""
-    num1**3
+    return num1**3
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-    num1**num2
+    return num1**num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
-    num1 % num2
+    return num1 % num2
 
 def add_mult(num1, num2, num3): 
     """Return the sum of num1, num2, and num3"""
